99ab6712b15fe25c7efbd94d59d947db6aa3e149
kotasai98 <seshasaikota20@gmail.com>
2023-02-15 21:37:58 -0500

    my first commit

--- src/main/java/com/dssc/training/Main.java
+++ src/main/java/com/dssc/training/Main.java
@@ -3,6 +3,6 @@ package com.dssc.training;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World-sai");
     }
 }
