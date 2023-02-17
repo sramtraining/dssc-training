d0322baa9c133636a9465a7a224ec95313e6c461
sripatelchinthala <chinthalasrikanth9532@gmail.com>
2023-02-17 13:46:33 -0500

    my commit 1

--- src/main/java/com/dssc/training/Main.java
+++ src/main/java/com/dssc/training/Main.java
@@ -6,5 +6,8 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("vamshi initial commit");
         System.out.println("vamshi Second commit");
+        int a =5;
+        int b = 5;
+        System.out.println(a+b);
     }
 }
