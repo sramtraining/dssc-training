5706d3c2ad1a78579b15fede300745143d658ab4
gopi5014 <gopi.gu5014@gmail.com>
2023-02-16 22:30:14 -0500

    my first commit

--- src/main/java/com/dssc/training/Main.java
+++ src/main/java/com/dssc/training/Main.java
@@ -3,6 +3,12 @@ package com.dssc.training;
 public class Main {
 
     public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
         System.out.println("Hello World");
+        System.out.println("Hi I am Gopi");
+
     }
 }
