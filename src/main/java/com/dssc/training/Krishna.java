317dc8c22e99b4b6e7b5be8a61286f012f6ba70e
krishna77339 <47357662+krishna77339@users.noreply.github.com>
2023-02-22 02:15:30 -0500

    Add files via upload

---
+++ src/main/java/com/dssc/training/Krishna.java
@@ -0,0 +1,11 @@
+package com.dssc.training;
+
+public class Krishna {
+    public static void main(String args[])
+    {
+        int a=20,b=30,c;
+        c = a+b;
+        System.out.println("Addition = "+c);
+    }
+
+}
